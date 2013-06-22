267d8e58455c38301fab70a9154a203bb3a4ef7a
kimjw <kimjw@kimjw-PC>
2013-06-22 10:42:11 +0900

    second

--- kimTest1234/src/com/example/kimtest1234/MainActivity.java
+++ kimTest1234/src/com/example/kimtest1234/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.kimtest1234;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.v("aa", "aa");
 	}
 
 	@Override
